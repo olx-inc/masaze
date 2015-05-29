6f53a14fb389352b7fd04708219430f67dbaa798
Gonzalo Espinosa <gonzaloe@olx.com>
2015-05-29 11:49:48 -0300

    Adding Appointments table

--- db/masaze_ddl.sql
+++ db/masaze_ddl.sql
@@ -11,3 +11,10 @@ CREATE TABLE `masaze_users` (
   PRIMARY KEY (`id`),
   KEY `idx-email` (`email`)
 ) ENGINE=InnoDB;
+
+-- CREATE APPOINTMENTS TABLE
+
+CREATE TABLE `masaze_appointments` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB;
