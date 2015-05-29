2febaf950e2ad1c72c3dc9c45b0c4458b90cb37e
OLX - Santiago Bonacalza <dev@OLXs-MacBook-Pro-3.local>
2015-05-29 14:53:51 -0300

    Mails send cron commit

---
+++ db/dbConnection.php
@@ -0,0 +1,54 @@
+<?php
+
+class dbConnection {
+
+    protected static $instance;
+
+    protected $link;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new dbConnection();
+        }
+        return self::$instance;
+    }
+
+    public function executeQuery($query)
+    {
+        $result = array();
+
+        $this->getDbConnection();
+        $results = $this->link->query($query)->fetch_all();
+
+        foreach ($results as $data) {
+            foreach ($data as $key => $value) {
+                $result[] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    private function getDbConnection()
+    {
+        $dbCred = $this->getDbCredentials();
+
+        if (!$this->link = new mysqli($dbCred['HOST'], $dbCred['USER'], $dbCred['PWD'], $dbCred['DBNAME'])) {
+            echo 'Could not connect to mysql';
+            exit;
+        }
+    }
+
+    private function getDbCredentials()
+    {
+        $cred = array();
+
+        $cred['HOST'] = '10.4.12.27';
+        $cred['DBNAME'] = 'DB_MASAZE';
+        $cred['USER'] = 'mtd';
+        $cred['PWD'] = '_m7D#2015$';
+
+        return $cred;
+    }
+}
