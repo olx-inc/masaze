1dc558d45d4842d65e1861f7685828dab268fbc1
Gonzalo Espinosa <gonzaloe@olx.com>
2015-05-29 11:30:52 -0300

    Fix type

--- db/masaze_ddl.sql
+++ db/masaze_ddl.sql
@@ -1,14 +1,13 @@
 -- CREATE DB
 
-CREATE DATABASE DB_MASSAZE;
+CREATE DATABASE DB_MASAZE;
+USE DB_MASAZE;
 
 -- CREATE USERS TABLE
 
-CREATE TABLE `massaze_users` (
+CREATE TABLE `masaze_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx-email` (`email`)
 ) ENGINE=InnoDB;
-
-
